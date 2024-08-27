00818803d5533c04286cf748b3b43a90f41d0729
Vincenzo-Vengeance <134833962+Vincenzo-Vengeance@users.noreply.github.com>
2024-08-28 01:16:02 +0300

    Add files via upload

---
+++ none.py
@@ -0,0 +1,13 @@
+multipl = 1
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    else:
+        i % 2 != 0
+        multipl *= i
+print("Произведение нечетных чисел", multipl)
+        
+
+    
+    
+    
